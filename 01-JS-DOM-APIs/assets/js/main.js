4a1755808052d0797fa62bfa631b9ea1e664630a
vickydr04 <vickydr04@gmail.com>
2016-09-19 18:56:52 -0300

    ejercicios 7, 8 y 9. fueron realizados con ajax

--- 01-JS-DOM-APIs/assets/js/main.js
+++ 01-JS-DOM-APIs/assets/js/main.js
@@ -1,3 +1,18 @@
-$(document).ready(function(){
-    $(".hidden").fadeIn(1200);
+$(document).ready(function() {
+    $(".hidden").fadeIn(600, function() {
+        $(".alias").focus();
+    })
+    $("button").click(function() {
+        $.ajax({
+            url: "http://api.icndb.com/jokes/random",
+            success: function(data) {
+                if (data.type == 'success') {
+                    $('.hidden').html('<p>' + data.value.joke + '</p>');
+                }
+            },
+            error: function(data) {
+                $('.hidden').css('background-color', 'red');
+            }
+        });
+    });
 });
