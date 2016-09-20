4090f77e2b036c69c7c736a8bfcfe90c89d75c2e
vickydr04 <vickydr04@gmail.com>
2016-09-19 23:28:38 -0300

    ejercicio 11: se levanta y muestra informacion de spotify de los albums de los Rolling Stones

--- 01-JS-DOM-APIs/assets/js/main.js
+++ 01-JS-DOM-APIs/assets/js/main.js
@@ -16,5 +16,39 @@ $(document).ready(function() {
             }
         });
     });
-
+    spotifySearch();
 });
+
+
+function spotifySearch() {
+    var data = {
+        q: 'Rolling Stones',
+        type: 'album'
+    }
+    $('#albums').append('<h1> Listen to the songs of ' + data.q + '</h1>');
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.spotify.com/v1/search',
+        contentType: 'application/json; charset=utf-8',
+        data: $.param(data),
+        success: function(data) {
+            console.log(data.q);
+
+            $.each(data.albums.items, function(i, album) {
+
+                showAlbum(album);
+
+            });
+        },
+        error: function(data) {
+
+        }
+    });
+}
+
+function showAlbum(album) {
+    $('#albums').append('<article><img src="' + album.images["0"].url + '"/>' +
+        '<p> Name: ' + album.name + '</p>' +
+        '<p>Type: ' + album.type + '</p>' +
+        '<a href="' + album.external_urls.spotify + '">URL</a></article>');
+}
